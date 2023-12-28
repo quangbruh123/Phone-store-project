8088ceb21dcaf714640e18e99e1cc90c57ca004f
quangbruh123 <20520716@gm.uit.edu.vn>
2023-12-28 22:40:33 +0700

    Thêm redux store

---
+++ client/src/store/cartReducer.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartReducer = createSlice({
+    name: "cart",
+    initialState: {
+        quantity: 0,
+        cartItems: [],
+    },
+    reducers: {
+        addItems: (state, action) => {
+            state.quantity += 1;
+        },
+        removeItems: (state, action) => {
+            state.quantity -= 1;
+        },
+        clearItems: (state, action) => {
+            state.quantity = 0;
+            state.cartItems = [];
+        },
+    },
+});
+
+export const { addItems, removeItems, clearItems } = cartReducer.actions;
+
+export default cartReducer.reducer;
+
+export const getQuantity = (state) => state.cart.quantity;
+export const getCartItems = (state) => state.cart.cartItems;
---
+++ client/src/utils/useFetchData.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import axiosInstance from "../api/axiosConfig";
+
+const useFetchData = (url, query, dataType = "none") => {
+    const [data, setData] = useState(dataType === "object" ? {} : []);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axiosInstance.get(url, {
+                    params: query,
+                    withCredentials: true,
+                });
+                setData(response.data);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [url, query]);
+
+    return { data, isLoading, error };
+};
+
+export default useFetchData;
