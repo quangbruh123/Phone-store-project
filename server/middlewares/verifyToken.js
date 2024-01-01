3081d9ccc70bac8b02d1c041d1d76f0198acd107
thaiduong1705 <thaiduong7v@gmail.com>
2024-01-01 14:22:52 +0700

    sửa lỗi ở verify token

--- server/middlewares/verifyToken.js
+++ server/middlewares/verifyToken.js
@@ -4,29 +4,29 @@ const CustomAPIError = require("../error/customError");
 const verifyAccessToken = async (req, res, next) => {
 	const authToken = req.headers.authorization;
 
-	if (!authToken || !authToken.startsWith("Bearer ")) {
-		throw new CustomAPIError("Không thể lấy access token", 401);
-	}
-
-	const accessToken = authToken.split(" ")[1];
-
 	try {
+		if (!authToken || !authToken.startsWith("Bearer ")) {
+			next(new CustomAPIError("Không thể lấy access token", 401));
+		}
+
+		const accessToken = authToken.split(" ")[1];
+
 		const decode = jwt.verify(accessToken, process.env.SECRET_KEY);
 		req.user = { uid: decode._id, roleId: decode._roleId };
 		next();
 	} catch (error) {
-		throw new CustomAPIError("Access token đã hết hạn", 401);
+		next(new CustomAPIError("Access token đã hết hạn", 401));
 	}
 };
 
 const verifyRefreshToken = async (req, res, next) => {
 	const refreshToken = req.headers.authorization;
 
-	if (!refreshToken) {
-		throw new CustomAPIError("Không thể lấy refresh token", 401);
-	}
-
 	try {
+		if (!refreshToken) {
+			next(new CustomAPIError("Không thể lấy refresh token", 401));
+		}
+
 		const decode = jwt.verify(refreshToken, process.env.SECRET_KEY);
 		req.user = { uid: decode._id, roleId: decode._roleId, refreshToken };
 		next();
