96d37d478852fa2491836bc190f93447655b19f9
Thai Duong <thaiduong7v@gmail.com>
2024-01-03 23:45:25 +0700

    sửa 1 chút về rate

--- server/routes/phone.js
+++ server/routes/phone.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const { getAllPhone, getFilterProduct, getOnePhone, createPhone, updatePhone, deletePhone } = require("../controllers/phone");
-const { verifyAccessToken, checkIsStaffOrAdmin } = require("../middlewares/verifyToken");
+const { getAllPhone, getFilterProduct, getOnePhone, createPhone, updatePhone, deletePhone, rate } = require("../controllers/phone");
+const { verifyAccessToken, checkIsStaffOrAdmin, checkIsUser } = require("../middlewares/verifyToken");
 const uploadCloud = require("../config/cloudinary");
 const router = express.Router();
 
@@ -22,5 +22,5 @@ router
 	.get(verifyAccessToken, getOnePhone)
 	.put(verifyAccessToken, checkIsStaffOrAdmin, updatePhone)
 	.delete(verifyAccessToken, checkIsStaffOrAdmin, deletePhone);
-
+router.route("/rate").post(verifyAccessToken, rate);
 module.exports = router;
