84a9ab8995c0676c6d6a0e5aa7865040b2c21225
Thai Duong <thaiduong7v@gmail.com>
2024-01-03 11:54:27 +0700

    chỉnh lại phone

--- server/controllers/insert.js
+++ server/controllers/insert.js
@@ -6,7 +6,13 @@ const createSlug = require("../utils/createSlug");
 const insertPhones = asyncHandler(async (req, res) => {
 	for (phone of phoneJson) {
 		if (phone) {
-			await Phone.create({ ...phone, slug: createSlug(phone.phoneName), price: parseInt(phone.price.replace(".", "")) });
+			await Phone.create({
+				...phone,
+				slug: createSlug(phone.phoneName),
+				price: parseInt(phone.price.replace(".", "")) * 1000,
+				thumb: phone.imageLinks[0],
+				imageLinks: phone.imageLinks.slice(1),
+			});
 		}
 	}
 
