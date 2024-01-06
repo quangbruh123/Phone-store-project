e7f8c7d8890cba2b498febaec3325337492a0ded
Thai Duong <thaiduong7v@gmail.com>
2024-01-06 10:14:14 +0700

    làm wishlist bên be

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,35 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const {
-  getAllUser,
-  getCurrentUser,
-  getOneUser,
-  updateUser,
-  deleteUser,
-  updateUserByAdmin,
-  updateCart,
-  removeProductInCart,
-  replaceNewCart,
-} = require('../controllers/user');
-const {
-  checkIsStaffOrAdmin,
-  verifyAccessToken,
-  checkIsUser,
-} = require('../middlewares/verifyToken');
+	getAllUser,
+	getCurrentUser,
+	getOneUser,
+	updateUser,
+	deleteUser,
+	updateUserByAdmin,
+	updateCart,
+	removeProductInCart,
+	replaceNewCart,
+	updateWishlist,
+} = require("../controllers/user");
+const { checkIsStaffOrAdmin, verifyAccessToken, checkIsUser } = require("../middlewares/verifyToken");
 
-router.route('/').get(getAllUser);
-router
-  .route('/current')
-  .get(verifyAccessToken, getCurrentUser)
-  .put(verifyAccessToken, updateCart)
-  .delete(verifyAccessToken, replaceNewCart); //verifyToken
-router
-  .route('/:uid')
-  .get(verifyAccessToken, getOneUser)
-  .put(verifyAccessToken, updateUser)
-  .delete(verifyAccessToken, checkIsStaffOrAdmin, deleteUser);
-router
-  .route('/admin/:uid')
-  .put(verifyAccessToken, checkIsStaffOrAdmin, updateUserByAdmin);
+router.route("/").get(getAllUser);
+router.route("/current").get(verifyAccessToken, getCurrentUser).put(verifyAccessToken, updateCart).delete(verifyAccessToken, replaceNewCart); //verifyToken
+router.route("/:uid").get(verifyAccessToken, getOneUser).put(verifyAccessToken, updateUser).delete(verifyAccessToken, checkIsStaffOrAdmin, deleteUser);
+
+router.route("/wishlist/:pid").put(verifyAccessToken, checkIsUser, updateWishlist);
+router.route("/admin/:uid").put(verifyAccessToken, checkIsStaffOrAdmin, updateUserByAdmin);
 
 module.exports = router;
